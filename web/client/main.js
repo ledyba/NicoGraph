98e12e3743d38ddaab2376ebcebd54969b3cc555
Kaede Fujisaki <kaede@hexe.net>
2015-03-21 18:13:23 +0900

    improve graph

--- web/client/main.js
+++ web/client/main.js
@@ -51,8 +51,6 @@ $(function() {
       edges.add(payload.edges);
       var container = document.getElementById('visualize');
       var options = {
-        width:  '1200px',
-        height: '1200px',
         nodes: {
             shape: 'dot',
             radiusMin: 10,
@@ -63,7 +61,11 @@ $(function() {
             },
         edges: {
             width: 0.15,
-            inheritColor: "from"
+            inheritColor: "from",
+            color:{
+                color: 'rgba(150,150,150,0.3)',
+                highlight: 'rgba(43,124,233,0.5)'
+            }
             },
         tooltip: {
             delay: 200,
@@ -75,139 +77,43 @@ $(function() {
           stabilize: false,
         smoothCurves: {dynamic:false, type: "continuous"},
         physics: {barnesHut: {gravitationalConstant: -80000, springConstant: 0.001, springLength: 200}},
-        hideEdgesOnDrag: true
+        hideEdgesOnDrag: true,
+        smoothCurves: false
       };
       network = new vis.Network(container, datas, options);
       network.on("click",onClick);
+      network.on("doubleClick",onDoubleClick);
   }
 
+var nonselected = 'rgba(150,150,150,0.3)';
+var selected = {
+    background: '#97C2FC',
+    border: '#2B7CE9',
+};
 function onClick(selectedItems) {
-    var nodeId;
-    var degrees = 2;
-    // we get all data from the dataset once to avoid updating multiple times.
+    var fromId = selectedItems.nodes[0];
+    var connected = network.getConnectedNodes(fromId);
     var allNodes = nodes.get({returnType:"Object"});
-    if (selectedItems.nodes.length == 0) {
-        // restore on unselect
-        for (nodeId in allNodes) {
-            if (allNodes.hasOwnProperty(nodeId)) {
-                allNodes[nodeId].color = undefined;
-                if (allNodes[nodeId].oldLabel !== undefined) {
-                    allNodes[nodeId].label = allNodes[nodeId].oldLabel;
-                    allNodes[nodeId].oldLabel = undefined;
+    var changed = [];
+    for(nodeId in allNodes){
+        if(allNodes.hasOwnProperty(nodeId)){
+            var index = connected.indexOf(parseInt(nodeId));
+            if(nodeId !== fromId && index < 0){
+                if(allNodes[nodeId].color !== nonselected){
+                    allNodes[nodeId].color = nonselected;
+                    changed.push(allNodes[nodeId]);
                 }
-                allNodes[nodeId]['levelOfSeperation'] = undefined;
-                allNodes[nodeId]['inConnectionList'] = undefined;
-            }
-        }
-    }
-    else {
-        // we clear the level of separation in all nodes.
-        clearLevelOfSeperation(allNodes);
-
-        // we will now start to collect all the connected nodes we want to highlight.
-        var connectedNodes = selectedItems.nodes;
-
-        // we can store them into levels of separation and we could then later use this to define a color per level
-        // any data can be added to a node, this is just stored in the nodeObject.
-        storeLevelOfSeperation(connectedNodes,0, allNodes);
-        for (var i = 1; i < degrees + 1; i++) {
-            appendConnectedNodes(connectedNodes);
-            storeLevelOfSeperation(connectedNodes, i, allNodes);
-        }
-        for (nodeId in allNodes) {
-            if (allNodes.hasOwnProperty(nodeId)) {
-                if (allNodes[nodeId]['inConnectionList'] == true) {
-                    if (allNodes[nodeId]['levelOfSeperation'] !== undefined) {
-                        if (allNodes[nodeId]['levelOfSeperation'] >= 2) {
-                            allNodes[nodeId].color = 'rgba(150,150,150,0.75)';
-                        }
-                        else {
-                            allNodes[nodeId].color = undefined;
-                        }
-                    }
-                    else {
-                        allNodes[nodeId].color = undefined;
-                    }
-                    if (allNodes[nodeId].oldLabel !== undefined) {
-                        allNodes[nodeId].label = allNodes[nodeId].oldLabel;
-                        allNodes[nodeId].oldLabel = undefined;
-                    }
-                }
-                else {
-                    allNodes[nodeId].color = 'rgba(200,200,200,0.5)';
-                    if (allNodes[nodeId].oldLabel === undefined) {
-                        allNodes[nodeId].oldLabel = allNodes[nodeId].label;
-                        allNodes[nodeId].label = "";
-                    }
+            }else{
+                if(allNodes[nodeId].color !== selected){
+                    allNodes[nodeId].color = selected;
+                    changed.push(allNodes[nodeId]);
                 }
             }
         }
     }
-    var updateArray = [];
-    for (nodeId in allNodes) {
-        if (allNodes.hasOwnProperty(nodeId)) {
-            updateArray.push(allNodes[nodeId]);
-        }
-    }
-    nodes.update(updateArray);
+    nodes.update(changed);
 }
-/**
- * update the allNodes object with the level of separation.
- * Arrays are passed by reference, we do not need to return them because we are working in the same object.
- */
-function storeLevelOfSeperation(connectedNodes, level, allNodes) {
-    for (var i = 0; i < connectedNodes.length; i++) {
-        var nodeId = connectedNodes[i];
-        if (allNodes[nodeId]['levelOfSeperation'] === undefined) {
-            allNodes[nodeId]['levelOfSeperation'] = level;
-        }
-        allNodes[nodeId]['inConnectionList'] = true;
-    }
-}
-
-function clearLevelOfSeperation(allNodes) {
-    for (var nodeId in allNodes) {
-        if (allNodes.hasOwnProperty(nodeId)) {
-            allNodes[nodeId]['levelOfSeperation'] = undefined;
-            allNodes[nodeId]['inConnectionList'] = undefined;
-        }
-    }
-}
-
-/**
- * Add the connected nodes to the list of nodes we already have
- *
- *
- */
-function appendConnectedNodes(sourceNodes) {
-    var tempSourceNodes = [];
-    // first we make a copy of the nodes so we do not extend the array we loop over.
-    for (var i = 0; i < sourceNodes.length; i++) {
-        tempSourceNodes.push(sourceNodes[i])
-    }
-
-    for (var i = 0; i < tempSourceNodes.length; i++) {
-        var nodeId = tempSourceNodes[i];
-        if (sourceNodes.indexOf(nodeId) == -1) {
-            sourceNodes.push(nodeId);
-        }
-        var connectedNodes = network.getConnectedNodes(nodeId);
-        addUnique(connectedNodes,sourceNodes);
-    }
-    tempSourceNodes = null;
-}
-
-/**
- * Join two arrays without duplicates
- * @param fromArray
- * @param toArray
- */
-function addUnique(fromArray, toArray) {
-    for (var i = 0; i < fromArray.length; i++) {
-        if (toArray.indexOf(fromArray[i]) == -1) {
-            toArray.push(fromArray[i]);
-        }
-    }
+function onDoubleClick(selectedItems) {
 }
 });
 $(function() {
