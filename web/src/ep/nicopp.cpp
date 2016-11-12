ca087a50fadd8b2392530558521359b36960fc72
Kaede Fujisaki <kaede@hexe.net>
2016-11-12 18:13:43 +0900

    flags & logging

--- web/src/ep/nicopp.cpp
+++ web/src/ep/nicopp.cpp
@@ -28,6 +28,11 @@ using websocketpp::lib::bind;
 class Server;
 class Session;
 
+/** flags **/
+DEFINE_int32(port, 9002, "Port to listen on with HTTP protocol");
+DEFINE_string(cert, "server.pem", "Server Certificate File");
+DEFINE_string(privkey, "server.pem", "Server Private key File");
+
 /** For TLS **/
 typedef websocketpp::lib::shared_ptr<websocketpp::lib::asio::ssl::context> context_ptr;
 
@@ -38,8 +43,7 @@ std::string get_password() {
 context_ptr on_tls_init(websocketpp::connection_hdl hdl) {
     namespace asio = websocketpp::lib::asio;
 
-    std::cout << "on_tls_init called with hdl: " << hdl.lock().get() << std::endl;
-    std::cout << "using TLS mode: " << (mode == MOZILLA_MODERN ? "Mozilla Modern" : "Mozilla Intermediate") << std::endl;
+    LOG(INFO) << "on_tls_init called with hdl: " << hdl.lock().get();
 
     context_ptr ctx = websocketpp::lib::make_shared<asio::ssl::context>(asio::ssl::context::sslv23);
 
@@ -62,19 +66,15 @@ context_ptr on_tls_init(websocketpp::connection_hdl hdl) {
       std::string ciphers = "ECDHE-RSA-AES128-GCM-SHA256:ECDHE-ECDSA-AES128-GCM-SHA256:ECDHE-RSA-AES256-GCM-SHA384:ECDHE-ECDSA-AES256-GCM-SHA384:DHE-RSA-AES128-GCM-SHA256:DHE-DSS-AES128-GCM-SHA256:kEDH+AESGCM:ECDHE-RSA-AES128-SHA256:ECDHE-ECDSA-AES128-SHA256:ECDHE-RSA-AES128-SHA:ECDHE-ECDSA-AES128-SHA:ECDHE-RSA-AES256-SHA384:ECDHE-ECDSA-AES256-SHA384:ECDHE-RSA-AES256-SHA:ECDHE-ECDSA-AES256-SHA:DHE-RSA-AES128-SHA256:DHE-RSA-AES128-SHA:DHE-DSS-AES128-SHA256:DHE-RSA-AES256-SHA256:DHE-DSS-AES256-SHA:DHE-RSA-AES256-SHA:!aNULL:!eNULL:!EXPORT:!DES:!RC4:!3DES:!MD5:!PSK";
 
       if (SSL_CTX_set_cipher_list(ctx->native_handle() , ciphers.c_str()) != 1) {
-          std::cout << "Error setting cipher list" << std::endl;
+        LOG(ERROR) << "Error setting cipher list";
       }
     } catch (std::exception& e) {
-        std::cout << "Exception: " << e.what() << std::endl;
+      LOG(ERROR) << "Exception: " << e.what();
     }
     return ctx;
 }
 
 
-/**  **/
-DEFINE_int32(port, 9002, "Port to listen on with HTTP protocol");
-DEFINE_string(cert, "server.pem", "Server Certificate File");
-DEFINE_string(privkey, "server.pem", "Server Private key File");
 std::shared_ptr<nicopp::DataSet> dataSet;
 std::shared_ptr<Server> serv;
 
